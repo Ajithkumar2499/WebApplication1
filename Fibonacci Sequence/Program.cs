bf28b7efd9e71ff7c192605fb30547e56638b9df
Ajithkumar <ajithk2499@gmail.com>
2023-12-31 21:04:07 +0530

    Reverse string and Fibonacci sequence

---
+++ Fibonacci Sequence/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the data : ");
+            int input, n1 = 0, n2 = 1, result;
+            input = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= input; i++)
+            {
+                result = n1 + n2;
+                Console.Write(result + " ");
+                n1 = n2;
+                n2 = result;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Reverse string/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Reverse_string
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the data : ");
+            string input = Console.ReadLine();
+            string result = string.Empty;
+            for (int i = input.Length - 1; i > -1; i--)
+            {
+                result += input[i];
+            }
+            Console.Write($"Your reverse data is {result}");
+            Console.ReadLine();
+        }
+    }
+}
