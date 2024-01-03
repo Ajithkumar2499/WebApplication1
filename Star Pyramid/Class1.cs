c7adf4f2c62d43f7674277a051300b23994e1788
Ajithkumar <ajithk2499@gmail.com>
2024-01-03 20:00:20 +0530

    Star pyramid

---
+++ Star Pyramid/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Star_Pyramid
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input number of rows : ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= rows - i; j++)
+                    Console.Write(" ");
+                for (int j = 1; j <= 2 * i - 1; j++)
+                    Console.Write("*");
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
