3ccfdbf91157a664aa53a5f0a166a3e5d8508608
Ajithkumar <ajithk2499@gmail.com>
2024-01-04 13:13:03 +0530

    Factorial

---
+++ Factorial/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Factorial
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the data : ");
+            int input, result = 1;
+            input = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= input; i++)
+            {
+                result = result * i;
+            }
+            Console.Write(result);
+            Console.ReadLine();
+        }
+    }
+}
