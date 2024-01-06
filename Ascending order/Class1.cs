555c880fc82789aebc684e2c5ec89dcec10617ba
Ajithkumar <ajithk2499@gmail.com>
2024-01-06 15:00:15 +0530

    Ascending order

---
+++ Ascending order/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ascending_order
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { 5, 9, 2, 10, 1, 4 };
+            int result = 0;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                for (int j = i + 1; j < A.Length; j++)
+                {
+                    if (A[i] > A[j])
+                    {
+                        result = A[i];
+                        A[i] = A[j];
+                        A[j] = result;
+                    }
+                }
+            }
+            foreach (var item in A)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
