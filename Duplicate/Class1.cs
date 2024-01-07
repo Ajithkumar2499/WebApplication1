19cf8ad6526cce58d8a8d3e64c202ee305365495
Ajithkumar <ajithk2499@gmail.com>
2024-01-07 19:44:43 +0530

    Find the duplicate values using single for loop

---
+++ Duplicate/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Duplicate
+{
+    public class Class1
+    {
+        static void Main(string[] args)  //Find the duplicate values using single for loop
+        {
+            int[] s = { 1, 3, 5, 1, 2, 3 };
+            Array.Sort(s);
+            List<int> duplicates = new List<int>();
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i] == s[i - 1])
+                {
+                    duplicates.Add(s[i]);
+                }
+            }
+            Console.WriteLine("Duplicate elements are: " + String.Join(",", duplicates));
+            Console.ReadLine();
+        }
+    }
+}
--- Star Pyramid/Class1.cs
+++ Star Pyramid/Class1.cs
@@ -9,14 +9,14 @@ namespace Star_Pyramid
     {
         static void Main(string[] args)
         {
-            Console.Write("Input number of rows : ");
-            int rows = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input number of rows : ");
+            int rows = 5;
             for (int i = 1; i <= rows; i++)
             {
-                for (int j = 1; j <= rows - i; j++)
+                for (int j = 1; j <= rows -i; j++)
                     Console.Write(" ");
-                for (int j = 1; j <= 2 * i - 1; j++)
-                    Console.Write("*");
+                for (int j = 1; j <= i; j++)
+                    Console.Write("* ");
                 Console.Write("\n");
             }
             Console.ReadLine();
