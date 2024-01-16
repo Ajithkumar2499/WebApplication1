f765490875aae6a89622bf4ea6ff4968c94d0150
Ajithkumar <ajithk2499@gmail.com>
2024-01-16 12:34:29 +0530

    Duplicate LINQ

---
+++ DuplicateLINQ/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DuplicateLINQ
+{
+    public class Class1
+    {
+		public static void Main()
+		{
+			List<int> list = new List<int>() { 3, 5, 3, 2, 7, 7, 5, 6 };
+
+			IEnumerable<int> duplicates = list.GroupBy(x => x)
+			.Where(g => g.Count() > 1)
+			.Select(x => x.Key);
+
+			Console.WriteLine("Duplicate elements are: " + String.Join(",", duplicates));
+			Console.ReadLine();
+		}
+	}
+}
