0c4e282aa3c2ebf306ecf61cbd1c1c820abcbaa8
Ajithkumar <ajithk2499@gmail.com>
2024-01-22 12:35:08 +0530

    Multiply two numbers without using * operator

---
+++ Multiply/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Multiply
+{
+    public class Class1
+    {
+        //Multiply two numbers without using * operator
+        public static void Main()
+        {
+            int num1 = 5;
+            int num2 = 10;
+            int result = 0;
+
+            for (int i = 0; i < num2; i++)
+            {
+                int temp = result + num1;
+                result = temp;
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
