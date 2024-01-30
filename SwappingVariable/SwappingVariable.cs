48577e4abe32dcfd4065f42e28a2e4f36a84ad2e
Ajithkumar <ajithk2499@gmail.com>
2024-01-30 08:33:31 +0530

    Swapping the variables

---
+++ SwappingVariable/SwappingVariable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SwappingVariable
+{
+    public class SwappingVariable
+    {
+        public static void Main()
+        {
+            int a = 10;
+            int b = 50;
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine($"a = {a}");
+            Console.WriteLine($"b = {b}");
+            Console.Read();
+        }
+    }
+}
