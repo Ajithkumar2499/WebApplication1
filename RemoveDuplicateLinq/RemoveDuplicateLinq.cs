bcd70c2302709db417fc632f1452d6504f4c1789
Ajithkumar <ajithk2499@gmail.com>
2024-02-15 13:37:51 +0530

    Remove Duplicate using Linq query

---
+++ RemoveDuplicateLinq/RemoveDuplicateLinq.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RemoveDuplicateLinq
+{
+    public class RemoveDuplicateLinq
+    {
+        public static void Main()
+        {
+            int[] a = { 1, 2, 3, 4, 2, 3, 5, 6, 7, 1, 4, 8, 9, 9 };
+
+            IEnumerable<int> unique = a.Distinct<int>();
+            Console.Write(string.Join(" ", unique));
+            Console.Read();
+        }
+    }
+}
