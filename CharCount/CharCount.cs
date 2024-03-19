bac7493c32f0ecfc69057a6e0cd41a7a18400851
Ajithkumar <ajithk2499@gmail.com>
2024-03-19 08:16:50 +0530

    Character count

---
+++ CharCount/CharCount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CharCount
+{
+    public class CharCount
+    {
+        public static void Main()
+        {
+            string str = "AEROPLANE LANDING";
+
+            for(int i = 0; i < str.Length; i++)
+            {
+                int Count = 0;
+                for(int j = 0; j < str.Length; j++)
+                {
+                    if(str[i] == str[j])
+                    {
+                        Count++;
+                    }
+                }
+                Console.WriteLine($" {str[i]} is count {Count}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
