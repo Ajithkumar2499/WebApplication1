ba74471b67e58f26cabac04de7aa21089d29a087
Ajithkumar <ajithk2499@gmail.com>
2024-04-23 09:47:21 +0530

    Star Pyramid

--- Star Pyramid/Class1.cs
+++ Star Pyramid/Class1.cs
@@ -13,13 +13,37 @@ namespace Star_Pyramid
             int rows = 5;
             for (int i = 1; i <= rows; i++)
             {
-                for (int j = 1; j <= rows -i; j++)
+                for (int j = 1; j <= rows - i; j++)
                     Console.Write(" ");
                 for (int j = 1; j <= i; j++)
                     Console.Write("* ");
                 Console.Write("\n");
             }
             Console.ReadLine();
+            /*
+                        *
+                       * *
+                      * * *
+                     * * * *
+                    * * * * *
+             */
+            //-------------------------------
+            //for (int i = 1; i <= rows; i++)
+            //{
+            //    for (int j = 1; j <= rows - i; j++)
+            //        Console.Write(" ");
+            //    for (int j = 1; j <= 2 * i - 1; j++)
+            //        Console.Write("*");
+            //    Console.WriteLine();
+            //}
+            //Console.Read();
+            ///*
+            //        *
+            //       ***
+            //      *****
+            //     *******
+            //    *********
+            //*/
         }
     }
 }
