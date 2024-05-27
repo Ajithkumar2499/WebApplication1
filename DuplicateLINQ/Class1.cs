62ca57bc3d1389025450ad6a715568ff613c60b6
Ajithkumar <ajithk2499@gmail.com>
2024-05-27 11:36:39 +0530

    Duplicate with Count

--- DuplicateLINQ/Class1.cs
+++ DuplicateLINQ/Class1.cs
@@ -9,17 +9,26 @@ namespace DuplicateLINQ
     {
 		public static void Main()
 		{
-			List<int> list = new List<int>() { 3, 5, 3, 2, 7, 7, 5, 6 };
+			//List<int> list = new List<int>() { 3, 5, 3, 2, 7, 7, 5, 6 };
 
-			IEnumerable<int> duplicates = list.GroupBy(x => x)
+			//IEnumerable<int> duplicates = list.GroupBy(x => x)
+			//.Where(g => g.Count() > 1)
+			//.Select(x => x.Key);
+
+			//var result = list.Skip(3).Take(4).ToList();
+
+			//Console.WriteLine("Skip and Take: " + String.Join(",", result));
+
+			//Console.WriteLine("Duplicate elements are: " + String.Join(",", duplicates));
+			//Console.ReadLine();
+
+			//Duplicate with Count
+			int[] array = { 10, 5, 10, 2, 2, 3, 4, 5, 5, 6, 7, 8, 9, 11, 12, 12 };
+			var duplicate = array.GroupBy(x => x)
 			.Where(g => g.Count() > 1)
-			.Select(x => x.Key);
-
-			var result = list.Skip(3).Take(4).ToList();
-
-			Console.WriteLine("Skip and Take: " + String.Join(",", result));
-
-			Console.WriteLine("Duplicate elements are: " + String.Join(",", duplicates));
+			.Select(y => new { Item = y.Key, Count = y.Count() })
+			.ToList();
+			Console.WriteLine(String.Join("\n", duplicate));
 			Console.ReadLine();
 		}
 	}
